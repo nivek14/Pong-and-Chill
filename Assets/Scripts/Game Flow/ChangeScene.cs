2634ec653802fb4c81eafa1669270a68d49893dd
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2021-07-27 19:54:44 -0300

    Changes in Game Hub
    - Making scene manager become a prefab
    - Add buttons and text in Game hub scene

--- Assets/Scripts/Game Flow/ChangeScene.cs
+++ Assets/Scripts/Game Flow/ChangeScene.cs
@@ -4,10 +4,6 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ChangeScene : MonoBehaviour{
-
-    private void Awake() {
-        DontDestroyOnLoad(this.gameObject);
-    }
     
     public void LoadScene(string scene){
         SceneManager.LoadScene(scene, LoadSceneMode.Additive);
