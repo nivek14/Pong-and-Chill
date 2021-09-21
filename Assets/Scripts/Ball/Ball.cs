7ab52be7e28654934fc547fe6d949af78e7d3841
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2021-09-21 17:30:04 -0300

    Fix ball script

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour{
 
-    [SerializeField] private Vector2 speedRandX = new Vector2(1, 3);
-    [SerializeField] private Vector2 speedRandY = new Vector2(1, 3);
-    private Vector3 speed = new Vector3(1, 1);
+    //[SerializeField] private Vector2 speedRandX = new Vector2(1, 3);
+    //[SerializeField] private Vector2 speedRandY = new Vector2(1, 3);
+    [SerializeField] private Vector3 speed = new Vector3(0.5f, 0.5f);
     private Vector3 startSpeed;
     private Vector3 startPosition;
-    private bool canMove = false;
 
     void Awake(){
         startPosition = transform.position;
@@ -21,26 +20,28 @@ public class Ball : MonoBehaviour{
     }
 
     private void Movement(){
-        if(!canMove) return;
-        if(Input.GetKeyDown(KeyCode.T)) transform.Translate(speed);
+        transform.Translate(speed);
     }
 
     private void OnCollisionEnter2D(Collision2D other){
-        if(other.transform.tag == "Player") OnPlayerCollision();
-        else speed.y *= -1;
+        if(other.transform.tag == "Player"){
+            //OnPlayerCollision();
+            speed.y *= -1;
+        }
+        else speed.x *= -1; 
     }
 
-    private void OnPlayerCollision(){
+    /*private void OnPlayerCollision(){
 
-        float rand = Random.Range(speedRandX.x, speedRandY.y);
+        float rand = Random.Range(speedRandY.x, speedRandY.y);
 
-        if(speed.x < 0) rand = -rand; 
-        speed.x = rand;
-
-        rand = Random.Range(speedRandY.x, speedRandY.y);
+        if(speed.y > 0) rand = -rand; 
         speed.y = rand;
 
-    }
+        rand = Random.Range(speedRandX.x, speedRandX.y);
+        speed.x = rand;
+
+    }*/
 
     public void ResetBall(){
         transform.position = startPosition;    
