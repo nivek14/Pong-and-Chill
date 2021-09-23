81e66a5bce86d86f70a1e85911236e2fd33330d0
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2021-09-22 23:01:46 -0300

    Reset ball for original position

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour{
 
-    //[SerializeField] private Vector2 speedRandX = new Vector2(1, 3);
-    //[SerializeField] private Vector2 speedRandY = new Vector2(1, 3);
     [SerializeField] private Vector3 speed = new Vector3(0.5f, 0.5f);
     private Vector3 startSpeed;
     private Vector3 startPosition;
 
     void Awake(){
+        gameObject.SetActive(true);
         startPosition = transform.position;
         startSpeed = speed;
     }
@@ -24,24 +23,9 @@ public class Ball : MonoBehaviour{
     }
 
     private void OnCollisionEnter2D(Collision2D other){
-        if(other.transform.tag == "Player"){
-            //OnPlayerCollision();
-            speed.y *= -1;
-        }
+        if(other.transform.tag == "Player") speed.y *= -1;
         else speed.x *= -1; 
     }
-
-    /*private void OnPlayerCollision(){
-
-        float rand = Random.Range(speedRandY.x, speedRandY.y);
-
-        if(speed.y > 0) rand = -rand; 
-        speed.y = rand;
-
-        rand = Random.Range(speedRandX.x, speedRandX.y);
-        speed.x = rand;
-
-    }*/
 
     public void ResetBall(){
         transform.position = startPosition;    
--- Assets/Scripts/Game Manager/Score.cs
+++ Assets/Scripts/Game Manager/Score.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class Score : MonoBehaviour{
     
-    [SerializeField] private Player player;
-    [SerializeField] private IA opponent;
-    
+    private Player player;
+    private IA opponent;
+    private Ball ball;
+
+    private void Awake(){
+        player = FindObjectOfType<Player>();
+        opponent = FindObjectOfType<IA>();
+        ball = FindObjectOfType<Ball>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other){
-        if(other.transform.tag == "ScoreForPlayer") player._playerData.playerScore++;
-        else if(other.transform.tag == "ScoreForOpponent") opponent._IAscore++;
+        if(other.transform.tag == "ScoreForPlayer"){
+            player._playerData.playerScore++;
+            ball.ResetBall();
+        }
+        else if(other.transform.tag == "ScoreForOpponent"){
+            opponent._IAscore++;
+            ball.ResetBall();
+        }
     }
 
 }
